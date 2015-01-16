ff243a08ce2352fbfa52b93249d0fe411b0b6e5c
taavirehemagi <taavirehemagi@gmail.com>
2015-01-16 16:23:02 +0200

    Make widget tick and open app on button tap.

--- Joey/Net/HomescreenWidgetProvider.cs
+++ Joey/Net/HomescreenWidgetProvider.cs
@@ -1,67 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using Android.App;
 using Android.Appwidget;
 using Android.Content;
 using Android.OS;
-using Android.Runtime;
-using Android.Views;
 using Android.Widget;
-using Toggl.Joey.UI.Activities;
-using Toggl.Joey.UI.Components;
-using System.Timers;
+using Toggl.Phoebe.Data;
 
 namespace Toggl.Joey.Net
 {
-
     [BroadcastReceiver]
     [IntentFilter (new string [] { "android.appwidget.action.APPWIDGET_UPDATE" })]
     [MetaData ("android.appwidget.provider", Resource = "@xml/widget_info")]
 
     public class HomescreenWidgetProvider : AppWidgetProvider
     {
-        public void OnReceive(Context context, Intent intent) {
-            base.OnReceive(context, intent);
-        }
-       
-        public override void OnUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds)
+        private Context context;
+        public static readonly string ExtraAppWidgetIds = "appWidgetIds";
+
+        public override void OnReceive (Context context, Intent intent)
         {
-            context.StartService (new Intent (context, typeof (WidgetService)));
+            base.OnReceive (context, intent);
+        }
 
-            Timer timer = new Timer();
-
-            timer.ScheduleAtFixedRate(
-            new TimerTask() {
-            public void run() {
-                RunOnUiThread(new Runnable() {
-                    public void run() {
-                        imageView.setImageBitmap(bitmap);
-                    }
-                });
-            }
-            }, 5000, 5000);
-
-            WidgetService m = new WidgetService();
-
-//            if(widget == null){
-//                Initialize();
-//            }
-//            var wState = widget.GetRunningDuration();
-
-//            for(int i=0; i < appWidgetIds.Length; i++) {
-//                int appWidgetId = appWidgetIds[i];
-
-//                RemoteViews views = new RemoteViews(context.PackageName, Resource.Layout.homescreen_widget);
-//                Console.WriteLine("OnUpdate");
-
-//                var serviceIntent = new Intent (context, typeof (WidgetService));
-//                serviceIntent.PutExtra ("command", WidgetService.CommandStart);
-//                PendingIntent pendingIntent = PendingIntent.GetService (context, 0, serviceIntent, 0);
-//                pendingIntent.Send();
-//                appWidgetManager.UpdateAppWidget (appWidgetId, views);
-//            }
-        } 
+        public override void OnUpdate (Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds)
+        {
+            this.context = context;
+            var serviceIntent = new Intent (context, typeof (WidgetService));
+            serviceIntent.SetAction (WidgetService.CommandInitial);
+            var serviceBundle = new Bundle ();
+            serviceBundle.PutIntArray (ExtraAppWidgetIds, appWidgetIds);
+            serviceIntent.PutExtras (serviceBundle);
+            context.StartService (serviceIntent);
+        }
     }
 }
--- Joey/Net/WidgetService.cs
+++ Joey/Net/WidgetService.cs
@@ -1,17 +1,9 @@
-﻿
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
+﻿using System;
 using Android.App;
 using Android.Content;
 using Android.OS;
-using Android.Runtime;
-using Android.Views;
 using Android.Widget;
 using Android.Appwidget;
-using Toggl.Joey.UI.Components;
 using Toggl.Phoebe.Data;
 using XPlatUtils;
 using Android.Graphics;
@@ -26,31 +18,22 @@ namespace Toggl.Joey.Net
         private ActiveTimeEntryManager timeEntryManager;
         private RemoteViews remoteViews;
         private AppWidgetManager manager;
-        private ComponentName widgetComponent;
-        public static readonly string CommandStart = "start";
-        public static readonly string CommandStop = "stop";
+        private int[] appWidgetIds;
+        public const string WidgetCommand = "command";
+        public const string CommandInitial = "initial";
+        public const string CommandStart = "start";
+        public const string CommandStop = "stop";
 
         public override void OnStart (Intent intent, int startId)
         {
-            var command = intent.GetStringExtra("command");
+            base.OnStart (intent, startId);
             context = this;
-//            switch (command) {
-//                case CommandStop:
-//                    StopRunningTimeEntry();
-//                case CommandStart:
-//
-//            }
-//            if(command == "start"){
-//                StartLastTimeEntry();
-//            }
-            Pulse();
+            if (intent != null && intent.Action.Equals (CommandStart)) {
+                StartEntryAndTogglApp ();
+            }
+            appWidgetIds = intent.GetIntArrayExtra (HomescreenWidgetProvider.ExtraAppWidgetIds);
+            Pulse ();
         }
-
-        private async void SendMessages()
-        {
-            await Task.Delay(1000);
-        }
-
 
         private void EnsureAdapter()
         {
@@ -58,39 +41,61 @@ namespace Toggl.Joey.Net
                 timeEntryManager = ServiceContainer.Resolve<ActiveTimeEntryManager> ();
             }
 
-//            if (remoteViews == null) {
-             RefreshViews();
-//            }
+            if (manager == null) {
+                manager = AppWidgetManager.GetInstance (this.ApplicationContext);
+            }
+        }
+        private void StartEntryAndTogglApp()
+        {
 
-//            if(manager == null) {
-                manager = AppWidgetManager.GetInstance (this);
-//            }
-
-//            if(widgetComponent == null) {
-            widgetComponent = new ComponentName(this, "Toggl.Joey.Net.HomescreenWidgetProvider");
-//            }
-            Console.WriteLine("widgetComponent: {0}", widgetComponent);
         }
 
         private void AttachEvents()
         {
-//            remoteViews.SetOnClickPendingIntent (Resource.Id.WidgetActionButton, pendingIntent);
+
         }
 
         private async void Pulse ()
         {
-            Console.WriteLine("Pulse");
-            EnsureAdapter();
-            manager.UpdateAppWidget(widgetComponent, remoteViews);
+            RefreshViews ();
+
+            manager.UpdateAppWidget (appWidgetIds, remoteViews);
+            manager.NotifyAppWidgetViewDataChanged (appWidgetIds, remoteViews.LayoutId);
+
             await Task.Delay (TimeSpan.FromMilliseconds (1000));
             Pulse();
         }
 
-        private void StartLastTimeEntry()
+        private TimeEntryState CurrentState
+        {
+            get {
+                return timeEntryManager.Active.State;
+            }
+        }
+
+        private void RefreshViews ()
         {
             EnsureAdapter();
-            var activeTimeEntryData = timeEntryManager.Active;
-            Console.WriteLine("starting last entry");
+            RemoteViews views = new RemoteViews (context.PackageName, Resource.Layout.homescreen_widget);
+
+            if (CurrentState == TimeEntryState.Running) {
+                views.SetInt (Resource.Id.WidgetActionButton, "setBackgroundColor", Color.Red);
+                views.SetInt (Resource.Id.WidgetActionButton, "setText", Resource.String.TimerStopButtonText);
+            } else {
+                views.SetInt (Resource.Id.WidgetActionButton, "setBackgroundColor", Color.Green);
+                views.SetInt (Resource.Id.WidgetActionButton, "setText", Resource.String.TimerStartButtonText);
+            }
+            var startAppIntent = new Intent ("android.intent.action.MAIN");
+            startAppIntent.AddCategory ("android.intent.category.LAUNCHER");
+            startAppIntent.AddFlags (ActivityFlags.NoAnimation);
+            startAppIntent.SetComponent (new ComponentName (context.PackageName, "toggl.joey.ui.activities.MainDrawerActivity"));
+            var startBundle = new Bundle ();
+            startBundle.PutString ("testKey", "testValue");
+            startAppIntent.PutExtras (startBundle);
+            var pendingIntent = PendingIntent.GetActivity (context, 0, startAppIntent, 0);
+            views.SetOnClickPendingIntent (Resource.Id.WidgetActionButton, pendingIntent);
+            views.SetTextViewText (Resource.Id.WidgetDuration, CurrentDuration());
+            remoteViews = views;
         }
 
         private void StopRunningTimeEntry()
@@ -98,40 +103,16 @@ namespace Toggl.Joey.Net
             EnsureAdapter();
             var activeTimeEntryData = timeEntryManager.Active;
             activeTimeEntryData.StopTime = DateTime.Now;
-            Console.WriteLine("stopping last entry");
-        }
-
-        private TimeEntryState CurrentState()
-        {
-            return timeEntryManager.Active.State;
-        }
-
-        private void RefreshViews ()
-        {
-            EnsureAdapter();
-            Console.WriteLine("refreshing views");
-            RemoteViews views = new RemoteViews(context.PackageName, Resource.Layout.homescreen_widget);
-            var state = CurrentState();
-            if(state == TimeEntryState.Running) {
-                Console.WriteLine("Is running");
-                views.SetInt(Resource.Id.WidgetActionButton, "background", Color.Red);
-            } else {
-                Console.WriteLine("NOt running");
-                views.SetInt(Resource.Id.WidgetActionButton, "background", Color.LightGreen);
-            }
-            Console.WriteLine("duration:  {0}", CurrentDuration());
-            views.SetTextViewText (Resource.Id.WidgetDuration, CurrentDuration());
-            remoteViews = views;
         }
 
         private string CurrentDuration()
         {
-            if (CurrentState() != TimeEntryState.Running) {
+            if (CurrentState != TimeEntryState.Running) {
                 return "00:00:00";
             }
             var activeTE = timeEntryManager.Active;
-            var d = DateTime.Now - activeTE.StartTime;
-            return String.Format("{0}:{1}:{2}", d.Hours, d.Minutes, d.Seconds);
+            var duration = DateTime.Now - activeTE.StartTime;
+            return duration.ToString (@"hh\:mm\:ss");
         }
 
         public override IBinder OnBind (Intent intent)
